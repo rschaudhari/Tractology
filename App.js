3dc4e7f259aed6d597a260a8d5e713a7155373e5
rschaudhari <rschauhdary7219@gmail.com>
2022-10-14 08:26:40 +0530

    dashboard ui added Cards for chapters

--- App.js
+++ App.js
@@ -5,17 +5,12 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ChapterScreen from './src/chapterContent/ChapterScreen';
 import LoginScreen from './src/auth/LoginScreen';
 import { COLORS } from './src/constants/Global';
+import RegisterScreen from './src/auth/RegisterScreen';
 
 export default function App() {
 
   const Stack = createNativeStackNavigator();
-
-  // const defaultTheme = {
-  //   colors: {
-  //     background:'#fff',
-  //   }
-  // }
-
+  
   return (
     <NavigationContainer>
       <Stack.Navigator 
@@ -25,6 +20,7 @@ export default function App() {
         }}      
       >
         <Stack.Screen name="Login" component={LoginScreen} options={{title:'Sign in'}} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{title:'Register',headerBackVisible:false}} />
         <Stack.Screen name="Dashboard" component={Dashboard} options={{title:'Tractology'}} />
         <Stack.Screen name="ChapterScreen" component={ChapterScreen} options={{title:'Tractology'}} />
       </Stack.Navigator>
